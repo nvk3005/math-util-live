881ab512e8e27d04e0f0e9013bc0ac5865a206b8
nvk3005 <nguyenvankent3005@gmail.com>
2024-07-07 22:50:55 +0700

    Lan 2 chinh code

--- src/com/fu/mathutil/MathUtilLive.java
+++ src/com/fu/mathutil/MathUtilLive.java
@@ -3,8 +3,11 @@ package com.fu.mathutil;
 public class MathUtilLive {
 
     public static void main(String[] args) {
-        long result = MathUtil.getFactorial(25);
-        System.out.println("5!=120; actual: " + result);
+//        long result = MathUtil.getFactorial(25);
+//        System.out.println("5!=120; actual: " + result);
+
+//10:45pm 7/7/2024
+        2024System.out.println("0!=1; actual: " + MathUtil.getFactorial(0));
 
     }
 
