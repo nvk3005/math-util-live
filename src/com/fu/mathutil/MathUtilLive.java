91356fa38b04a522805786e5d11f8b07b3df42e2
nvk3005 <nguyenvankent3005@gmail.com>
2024-07-08 11:34:04 +0700

    lan thu 3 update code-README.md

--- src/com/fu/mathutil/MathUtilLive.java
+++ src/com/fu/mathutil/MathUtilLive.java
@@ -7,7 +7,7 @@ public class MathUtilLive {
 //        System.out.println("5!=120; actual: " + result);
 
 //10:45pm 7/7/2024
-        2024System.out.println("0!=1; actual: " + MathUtil.getFactorial(0));
+        System.out.println("0!=1; actual: " + MathUtil.getFactorial(0));
 
     }
 
